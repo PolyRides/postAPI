2562ddf05fb4bb6916b7ff75e9d087dccb358299
Wenmin He <wenmin.he518@gmail.com>
2018-05-02 21:22:57 -0700

    Remove both files

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
- 
-var app = express()
- 
-app.get('', function(req, res) {
-  res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
-})
- 
-app.listen(3000)
